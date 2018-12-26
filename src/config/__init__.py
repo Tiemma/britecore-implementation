cb3018231a2b2d7c2a455b6be48a95aba181ed64
blank <bakareemmanuel98@outlook.com>
2018-12-26 23:28:11 +0100

    Refactored configurations for flask application

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -17,6 +17,7 @@ class Config:
     SECRET_KEY = urandom(256)
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = urandom(256)
+    THREADS_PER_PAGE = 1
 
     # Flask restplus configs
     RESTPLUS_VALIDATE = True
@@ -29,8 +30,9 @@ class Config:
 
     # SQL Alchemy configs
     SQLALCHEMY_DATABASE_URI = "sqlite:///{db}".format(db=abspath(join(getenv('DB_LOCATION', '../database'), 'app.db')))
-    SQLALCHEMY_ECHO=True
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     def __repr__(self):
         return '<Config {}>'.format(Config.__dict__)
@@ -40,14 +42,21 @@ class Development(Config):
     """
     Development configuration
     """
-    THREADS_PER_PAGE = 1
+    FLASK_ENV = 'development'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///{db}".format(db=abspath(join(getenv('DB_LOCATION', '/tmp'), 'app.db')))
+
+    def __repr__(self):
+        return '<Development {}>'.format(Development.__dict__)
 
 
 class Testing(Config):
     """
-    Development configuration
+    Testing configuration
     """
-    THREADS_PER_PAGE = 1
+    FLASK_ENV='testing'
+
+    def __repr__(self):
+        return '<Testing {}>'.format(Testing.__dict__)
 
 
 class Production(Config):
@@ -55,8 +64,13 @@ class Production(Config):
     Production configuration
     """
     DEBUG = False
+    FLASK_ENV='production'
     THREADS_PER_PAGE = 8
-    SQLALCHEMY_ECHO=False
+    SQLALCHEMY_ECHO = False
+    QUERYINSPECT_ENABLED = False
+
+    def __repr__(self):
+        return '<Production {}>'.format(Production.__dict__)
 
 
 CONFIG_BY_NAME = dict(
